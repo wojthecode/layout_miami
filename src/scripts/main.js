dec1928ae1d6fc92a952127341acfa8da186722d
Wojciech Witkowski <witus85@gmail.com>
2025-12-18 11:42:58 +0100

    added script preventing page reload after submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const contactForm = document.querySelector(".contact");
+contactForm.addEventListener("submit", function(f) {
+  f.preventDefault();
+
+  const formData = new FormData(contactForm);
+
+  fetch("#", { // <- server address
+    method: "POST",
+    body: formData
+  })
+
+  this.reset();
+});
